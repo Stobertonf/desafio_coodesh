46deb3ba31d6e0764052a2f53bb1f70cc2aea57e
stobertonf <stobertonf@gmail.com>
2023-07-04 20:16:20 -0300

    feat(Home-Page): Creating the ListWordsUseCase file.

---
+++ lib/app/modules/home/domain/usercases/list_word_usercase.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:desafio_coodesh/app/modules/core/failure.dart';
+import 'package:desafio_coodesh/app/modules/home/domain/repository/words_repository.dart';
+
+@Injectable()
+class ListWordsUseCase {
+  ListWordsUseCase(this.repository);
+  final IWordsRepository repository;
+
+  Future<Either<Failure, String>> call({
+    required String word,
+    required String results,
+    required String syllables,
+    required String pronunciation,
+    required String frequency,
+  }) async {
+    var result = await repository.listWords(
+      word: word,
+      results: results,
+      syllables: syllables,
+      pronunciation: pronunciation,
+      frequency: frequency,
+    );
+    return result;
+  }
+}
