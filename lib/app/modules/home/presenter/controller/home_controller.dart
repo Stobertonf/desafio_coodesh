f4e969ff65e236760602ec450c9da61fb9418529
stobertonf <stobertonf@gmail.com>
2023-07-04 20:19:17 -0300

    feat(Home-Page): Adding getWordDetails.

--- lib/app/modules/home/presenter/controller/home_controller.dart
+++ lib/app/modules/home/presenter/controller/home_controller.dart
@@ -2,7 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:desafio_coodesh/app/modules/core/failure.dart';
-import 'package:desafio_coodesh/app/modules/home/domain/repositories/words_repository.dart';
+import 'package:desafio_coodesh/app/modules/home/infra/models/word_details_model.dart';
+import 'package:desafio_coodesh/app/modules/home/domain/repository/words_repository.dart';
 import 'package:desafio_coodesh/app/modules/home/domain/usercases/save_word_usercase.dart';
 
 @Injectable()
@@ -10,19 +11,23 @@ class HomeController extends ChangeNotifier {
   final formKey = GlobalKey<FormState>();
 
   TextEditingController? textHelpController;
-  late final SaveWordsUseCase customerUseCase;
-  late final IWordsRepository customerRepository;
+  late final IWordsRepository wordsRepository;
+  late final SaveWordsUseCase saveWordsUseCase;
+  WordDetailsModel? _wordDetails;
 
-  HomeController(this.customerUseCase, this.customerRepository);
+  HomeController(
+    this.saveWordsUseCase,
+    this.wordsRepository,
+  );
 
-  Future<Either<Failure, String>> saveCustomer({
+  Future<Either<Failure, String>> saveWords({
     required String word,
     required String results,
     required String syllables,
     required String pronunciation,
     required String frequency,
   }) async {
-    var result = await customerUseCase.call(
+    var result = await saveWordsUseCase.call(
       word: word,
       results: results,
       syllables: syllables,
@@ -42,6 +47,32 @@ class HomeController extends ChangeNotifier {
     }
   }
 
+  Future<void> getWordDetails({
+    required String word,
+    String results = "",
+    String syllables = "",
+    String pronunciation = "",
+    String frequency = "",
+  }) async {
+    final result = await saveWordsUseCase.call(
+      word: word,
+      results: results,
+      syllables: syllables,
+      pronunciation: pronunciation,
+      frequency: frequency,
+    );
+
+    result.fold(
+      (failure) {
+        print("Falha ao capturar palavra: $failure");
+      },
+      (wordDetails) {
+        _wordDetails = wordDetails;
+        notifyListeners();
+      },
+    );
+  }
+
   String? validateWord(String? value) {
     RegExp onlyLetters = RegExp(r'^[a-zA-Z~ ]+$');
     if (value == null || value.isEmpty) {
@@ -49,7 +80,7 @@ class HomeController extends ChangeNotifier {
     } else if (!onlyLetters.hasMatch(value)) {
       return 'A palavra só pode conter letras';
     } else if (value.length > 98) {
-      return 'O palavra deve ter no máximo 98 caracteres.';
+      return 'A palavra deve ter no máximo 98 caracteres.';
     }
     return null;
   }
