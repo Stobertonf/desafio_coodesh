05b3799b70820525f2e1a0023fa17124b4f548a2
stobertonf <stobertonf@gmail.com>
2023-07-04 20:18:15 -0300

    feat(Home-Page): Creating the IWordsRepository.

---
+++ lib/app/modules/home/domain/repository/words_repository.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:desafio_coodesh/app/modules/core/failure.dart';
+
+abstract class IWordsRepository {
+  Future<Either<Failure, String>> saveWord({
+    required String word,
+    required String results,
+    required String syllables,
+    required String pronunciation,
+    required String frequency,
+  });
+
+  Future<Either<Failure, String>> listWords({
+    required String word,
+    required String results,
+    required String syllables,
+    required String pronunciation,
+    required String frequency,
+  });
+}
