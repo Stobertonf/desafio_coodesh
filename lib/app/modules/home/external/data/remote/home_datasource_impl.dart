6a99ccdb91116e24e6b0b31ac973c3f9c6afa9f4
stobertonf <stobertonf@gmail.com>
2023-07-08 14:02:06 -0300

    feat(Home-Page): Changing the HomeDataSourceImpl file.

--- lib/app/modules/home/external/data/remote/home_datasource_impl.dart
+++ lib/app/modules/home/external/data/remote/home_datasource_impl.dart
@@ -1,17 +1,19 @@
 import 'dart:convert';
+import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:desafio_coodesh/app/modules/core/failure.dart';
+import 'package:desafio_coodesh/app/modules/home/infra/data/home_datasource.dart';
+import 'package:desafio_coodesh/app/modules/home/domain/repository/home_repository.dart';
 
 @Injectable()
-abstract class WordsApiRepository {
-  Future<Map<String, dynamic>> getWordDetails(String word);
-}
-
-class HomeRepositoryImpl implements WordsApiRepository {
+class HomeDataSourceImpl extends HomeDataSource {
   final String baseUrl = "https://wordsapiv1.p.mashape.com/words";
 
-  @override
-  Future<Map<String, dynamic>> getWordDetails(String word) async {
+  HomeDataSourceImpl(HomeRepository homeRepository)
+      : super(homeRepository as HomeDataSourceImpl);
+
+  Future<Map<String, dynamic>> wordDetails(String word) async {
     final response = await http.get(
       Uri.parse('$baseUrl/$word'),
       headers: {
@@ -23,6 +25,75 @@ class HomeRepositoryImpl implements WordsApiRepository {
       return json.decode(response.body);
     } else {
       throw Exception("Failed to load data");
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> listWords() async {
+    try {
+      final response = await http.get(
+        Uri.parse(baseUrl),
+        headers: {
+          'X-Mashape-Key': 'YOUR_API_KEY',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return Right(data);
+      } else {
+        throw Exception("Failed to load data");
+      }
+    } catch (e) {
+      return Left(
+        Failure(message: "Failed to load data"),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> saveWords() async {
+    try {
+      final response = await http.get(
+        Uri.parse(baseUrl),
+        headers: {
+          'X-Mashape-Key': 'YOUR_API_KEY',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return Right(data);
+      } else {
+        throw Exception("Failed to load data");
+      }
+    } catch (e) {
+      return Left(
+        Failure(message: "Failed to load data"),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> getWordDetails() async {
+    try {
+      final response = await http.get(
+        Uri.parse(baseUrl),
+        headers: {
+          'X-Mashape-Key': 'YOUR_API_KEY',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return Right(data);
+      } else {
+        throw Exception("Failed to load data");
+      }
+    } catch (e) {
+      return Left(
+        Failure(message: "Failed to load data"),
+      );
     }
   }
 }
